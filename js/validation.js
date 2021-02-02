a8e0d47d3da92e02d362e938bf770479e27d3803
akomis <akomis01@ucy.ac.cy>
2021-02-02 12:26:43 +0200

    added robust validation with feedback

--- js/validation.js
+++ js/validation.js
@@ -1,7 +1,15 @@
 var form = document.querySelector('.needs-validation');
 
 form.addEventListener('submit', function(event) {
-    if (form.checkValidity() === false) {
+    requiredFields = form.querySelectorAll('[required]')
+
+    for (i = 0; i < requiredFields.length; i++) {
+        if (requiredFields[i].value === '') {
+            requiredFields[i].classList.add('is-invalid')
+        }
+    }
+
+    if (form.getElementsByClassName('is-invalid').length != 0) {
         event.preventDefault();
         event.stopPropagation();
     }
