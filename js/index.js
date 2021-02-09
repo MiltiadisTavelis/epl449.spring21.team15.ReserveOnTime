b0f41d727958932dac107e65f7df391006dc6b4e
akomis <akomis01@ucy.ac.cy>
2021-02-10 00:17:36 +0200

    added api call for searching

--- js/index.js
+++ js/index.js
@@ -12,8 +12,37 @@ $('.clockpicker').clockpicker({
     donetext: ''
 });
 
-let currentDate = new Date()
-let cDay = currentDate.getDate()
-let cMonth = currentDate.getMonth() + 1
-let cYear = currentDate.getFullYear()
-document.getElementById("dateInput").value = cDay + '/' + cMonth + '/' + cYear
+const submitButton = document.getElementById('submit-button');
+submitButton.onclick = submit;
+
+function submit() {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var xhr = new XMLHttpRequest();
+    var data = {
+        type: "shops/all"
+    };
+
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+            var message = document.createElement('div');
+            message.setAttribute("class", "alert alert-danger text-center");
+            message.setAttribute("role", "alert");
+            message.textContent = xhr.responseText;
+            var container = document.getElementById("contents1");
+            container.appendChild(message);
+
+            $(".alert-danger").delay(4000)
+                .fadeOut(function() {
+                    $(this).remove();
+                });
+        } else {
+            console.log(xhr.responseText);
+        }
+    }
+
+    xhr.open("GET", "http://api.reserveontime.com/action");
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+}
