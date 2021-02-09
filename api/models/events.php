a347d8d801074501428cc3d8f54459c7d029ddd1
cpapae02 <71434676+cpapae02@users.noreply.github.com>
2021-02-09 03:19:01 +0200

    Converted more event Calls to procedures
    update
    delete

--- api/models/events.php
+++ api/models/events.php
@@ -32,19 +32,6 @@
 				return false;
 			}
 		}
-		/*//SHOW EVENTS BY shop_id
-		public function shop_event(){
-			$sql = 'SELECT title,content,pic,link,start_date,stop_date  FROM EVENTS WHERE EVENTS.shop_id = '.$this->shop_id.' ORDER BY start_date ';
-			$stmt = $this->conn->prepare($sql);
-            if(!mysqli_stmt_prepare($stmt,$sql)){
-                echo "Error";
-                exit();
-            }else{
-            	mysqli_stmt_execute($stmt);
-            	$result = mysqli_stmt_get_result($stmt);
-            	return $result;
-        	}
-		}*/
 
 		
 		//CREATE NEW EVENT
@@ -59,7 +46,6 @@
 			$sql = 'CALL create_event("'.$this->title.'","'.$this->content.'","'.$this->pic.'","'.$this->link.'","'.$this->start_date.'","'.$this->stop_date.'",'.$this->shop_id.')';
 			
 			$stmt = $this->conn->prepare($sql);
-			echo $sql;
 			if($stmt->execute()){
 				return true;
 			}else{
@@ -70,55 +56,35 @@
 
 		//DELETE EVENT BY ID
 		public function delete_event(){
-			$sql = 'DELETE FROM EVENTS WHERE id=?';
-			$stmt = $this->conn->prepare($sql);
 			$this->id = htmlspecialchars(strip_tags($this->id));
-			$stmt->bind_param('i',$this->id);
-
+			$sql = 'CALL delete_event('.$this->id.')';
+			$stmt = $this->conn->prepare($sql);
 			if($stmt->execute()){
 				return true;
+			}else{
+				printf("Error: %s.\n",$stmt->error);
+				return false;
 			}
-
-			printf("Error: %s.\n",$stmt->error);
-
-			return false;
 		}
 
 		//UPDATE EVENT DETAILS BY ID
 		public function update_event(){
-			$sql = 'UPDATE EVENTS SET
-							title = ?,
-							content = ?,
-							pic = ?,
-							link = ?,
-							start_date = ?,
-							stop_date = ?
-							WHERE id=?';
-			$stmt = $this->conn->prepare($sql);
-			$this->id = htmlspecialchars(strip_tags($this->id));
 			$this->title = htmlspecialchars(strip_tags($this->title));
 			$this->content = htmlspecialchars(strip_tags($this->content));
 			$this->pic = htmlspecialchars(strip_tags($this->pic));
 			$this->link = htmlspecialchars(strip_tags($this->link));
 			$this->start_date = htmlspecialchars(strip_tags($this->start_date));
 			$this->stop_date = htmlspecialchars(strip_tags($this->stop_date));
-
-			$stmt->bind_param('ssssssi',
-										$this->title,
-										$this->content,
-										$this->pic,
-										$this->link,
-										$this->start_date,
-										$this->stop_date,
-										$this->id);
-
+			$this->id = htmlspecialchars(strip_tags($this->id));
+			$sql = 'CALL update_event("'.$this->title.'","'.$this->content.'","'.$this->pic.'","'.$this->link.'","'.$this->start_date.'","'.$this->stop_date.'","'.$this->id.'")';
+			echo $sql;
+			$stmt = $this->conn->prepare($sql);
 			if($stmt->execute()){
 				return true;
+			}else{
+				printf("Error: %s.\n",$stmt->error);
+				return false;
 			}
-
-			printf("Error: %s.\n",$stmt->error);
-
-			return false;
 		}
 	}
 ?>
