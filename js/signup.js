0b22fd693fac6bb8fd63a62012c756ff4cd518fd
cpapae02 <71434676+cpapae02@users.noreply.github.com>
2021-03-12 14:23:07 +0200

    Update signup.js
    user story signup implementation

--- js/signup.js
+++ js/signup.js
@@ -1,6 +1,51 @@
+var api = ""; // your xampp path to action.php example: "http://192.168.64.4/api/v1/action"
 $('.datepicker').datepicker({
     format: 'dd/mm/yyyy',
     weekStart: 1,
     endDate: "today",
     startView: 2
 });
+
+function submit() {
+    var name = document.getElementById('firstname-input');
+    var surname = document.getElementById('lastname-input');
+    var gender = document.getElementById('gender-input');
+    var birthday = document.getElementById('date-input');
+    var code = document.getElementById('telcode-input');
+    var number = document.getElementById('tel-input');
+    var email = document.getElementById('email-input');
+    var password = document.getElementById('password-input');
+    var xhr = new XMLHttpRequest();
+    var data = {
+        "type": "users/cuser",
+        "fname": name.value,
+        "lname": surname.value,
+        "birth": birthday.value,
+        "gender": gender.value,
+        "phone_code": code.value,
+        "pnum": number.value,
+        "email": email.value,
+        "password": password.value
+    };
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+
+            if (response.hasOwnProperty("status")) {
+                popUpMessage(response["message"], "success");
+                window.setTimeout(function() {
+                    window.location = "signin.html";
+                }, 1000);
+            } else {
+                popUpMessage(response["message"], "danger");
+            }
+        } else {
+            popUpMessage("Unexpected error", "danger");
+        }
+    }
+    xhr.withCredentials = true;
+    xhr.open('POST', api);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+}
