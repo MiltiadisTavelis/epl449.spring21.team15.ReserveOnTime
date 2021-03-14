d0d4a94e2a139dab2909fb0b692c397525766bbc
cpapae02 <71434676+cpapae02@users.noreply.github.com>
2021-03-14 16:19:17 +0200

    updated sql call for create in pending shops

--- api/v1/pendingShops/create.php
+++ api/v1/pendingShops/create.php
@@ -6,6 +6,7 @@
 		return;
 	};
 
+
 	//include_once '../../config/config.php';
 	//include_once '../../models/pendingShops.php';
 
@@ -15,11 +16,21 @@
 
 	$data = json_decode(file_get_contents("php://input"));
 
+	$pshop->fname = $data->fname;
+	$pshop->lname = $data->lname;
+	$pshop->gender = $data->gender;
+	$pshop->phone_code = $data->phone_code;
+	$pshop->number = $data->number;
+	$pshop->personal_email = $data->personal_email;
 	$pshop->sname = $data->sname;
 	$pshop->stype = $data->stype;
-	$pshop->email = $data->email;
-	$pshop->name = $data->name;
-	$pshop->pnum = $data->pnum;
+	$pshop->city = $data->city;
+	$pshop->province = $data->province;
+	$pshop->address = $data->address;
+	$pshop->postcode = $data->postcode;
+	$pshop->phone_code2 = $data->phone_code2;
+	$pshop->shop_number = $data->shop_number;
+	$pshop->shop_email = $data->shop_email;
 
 	if($pshop->create_pendingShop()){
 		$msg['status'] = 'Successfully Created';
