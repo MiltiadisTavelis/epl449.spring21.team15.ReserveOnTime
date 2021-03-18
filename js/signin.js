09f07fac3b3381b9b2dd1ab3b1c8b5d556d55e63
cpapae02 <71434676+cpapae02@users.noreply.github.com>
2021-03-18 17:25:36 +0200

    added function for creating cookies

--- js/signin.js
+++ js/signin.js
@@ -2,12 +2,14 @@
 function submit() {
     var email = document.getElementById('email-input');
     var pass = document.getElementById('password-input');
+    var remember = document.getElementById('remember-me');
     var xhr = new XMLHttpRequest();
     var data = {
         "type": "session/login",
         "email": email.value,
         "password": pass.value
     };
+    //console.log(remember.value);
 
     xhr.onload = function() {
         if (xhr.status === 200) {
@@ -16,10 +18,14 @@ function submit() {
             if (response["status"] === 0) {
                 popUpMessage(response["message"], "danger");
             } else if (response["status"] === 1) {
+                if (remember.value === "on") {
+                    
+                    create_cookies();
+                }
                 popUpMessage(response["message"], "success");
-                window.setTimeout(function() {
+               /* window.setTimeout(function() {
                     window.location = "index.html";
-                }, 1000);
+                }, 1000);*/
             } else if(response["status"] === 3){
                 popUpMessage(response["message"], "danger");
             }
@@ -27,8 +33,30 @@ function submit() {
             popUpMessage("Unexpected error", "danger");
         }
     }
+
+
     xhr.withCredentials = true;
     xhr.open('POST', api);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(data));
+}
+
+function create_cookies() {
+
+    var email = document.getElementById('email-input');
+    var pass = document.getElementById('password-input');
+    var xhr = new XMLHttpRequest();
+    var data = {
+        "type": "session/cookie",
+        "email": email.value,
+        "password": pass.value
+    };
+console.log(data);
+    xhr.onload = function() {
+        }
+    xhr.withCredentials = true;
+    xhr.open('POST', api);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+
 }
