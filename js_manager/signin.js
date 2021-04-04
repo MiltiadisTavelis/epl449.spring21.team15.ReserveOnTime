ad6b1ee3c4bda48f2283b347d02f6e5dd35dde4b
MiltiadisTavelis <mtavel01@ucy.ac.cy>
2021-04-04 18:34:37 +0300

    Manager sign in javascript

---
+++ js_manager/signin.js
@@ -0,0 +1,37 @@
+
+function submit() {
+    var email = document.getElementById('email-input');
+    var pass = document.getElementById('password-input');
+    var remember = document.getElementById('remember-me');
+    var xhr = new XMLHttpRequest();
+    var data = {
+        "type": "session/manager",
+        "email": email.value,
+        "password": pass.value
+    };
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+
+            if (response["status"] === 0) {
+                popUpMessage(response["message"], "danger");
+            } else if (response["status"] === 1) {
+                popUpMessage(response["message"], "success");
+               window.setTimeout(function() {
+                    window.location = "index.html";
+                }, 1000);
+            } else if(response["status"] === 3){
+                popUpMessage(response["message"], "danger");
+            }
+        } else {
+            popUpMessage("Unexpected error", "danger");
+        }
+    }
+
+
+    xhr.withCredentials = true;
+    xhr.open('POST', api);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+}
