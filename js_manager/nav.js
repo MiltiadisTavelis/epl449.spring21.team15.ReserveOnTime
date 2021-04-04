8cc6e2e45b864ca2d2c1bd6a9061b81748f6ab7b
akomis <akomis01@ucy.ac.cy>
2021-04-04 18:38:19 +0300

    added redir.js to redirect to index when no active session

--- js_manager/nav.js
+++ js_manager/nav.js
@@ -1,8 +1,6 @@
 const signout = document.getElementById('signout-btn');
 signout.onclick = logout;
 
-redirectEmptySession()
-
 function logout() {
     var xhr = new XMLHttpRequest();
     var data = {
@@ -17,28 +15,6 @@ function logout() {
         }
     }
 
-    xhr.withCredentials = true;
-    xhr.open('POST', api);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data));
-}
-
-function redirectEmptySession() {
-    var xhr = new XMLHttpRequest();
-    var data = {
-        "type": "session/islogin"
-    };
-
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            if (response["status"] === "0") {
-                window.location = "signin.html";
-            }
-        } else {
-            popUpMessage("There was an unexpected error", "danger");
-        }
-    }
     xhr.withCredentials = true;
     xhr.open('POST', api);
     xhr.setRequestHeader('Content-Type', 'application/json');
