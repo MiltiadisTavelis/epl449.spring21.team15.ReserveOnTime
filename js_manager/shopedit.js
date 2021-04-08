2e4407f83818f2fbc6076caa050f688c138bd7e3
akomis <akomis01@ucy.ac.cy>
2021-04-08 19:39:56 +0300

    added input for logo, thumbnail and photos as well as open hours

--- js_manager/shopedit.js
+++ js_manager/shopedit.js
@@ -1,1 +1,11 @@
 checkSession("m", true)
+
+window.addEventListener("load", function() {
+    var checkboxes = document.getElementsByClassName("check");
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener("change", function() {
+            let day = this.id.split("-")[1]
+            document.getElementById("time-" + day).disabled = this.checked
+        })
+    }
+})
