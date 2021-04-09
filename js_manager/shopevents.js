f133aa9f9e75dcb3f2bd4703a535ab65abf3d3b6
akomis <akomis01@ucy.ac.cy>
2021-04-09 13:41:39 +0300

    added shop events page

---
+++ js_manager/shopevents.js
@@ -0,0 +1,9 @@
+checkSession("m", true)
+
+$('.datepicker').datepicker({
+    format: 'dd/mm/yyyy',
+    weekStart: 1,
+    startDate: "today",
+    todayBtn: "linked",
+    todayHighlight: true
+});
