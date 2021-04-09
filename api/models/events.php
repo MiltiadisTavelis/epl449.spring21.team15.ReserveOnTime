dfc0334358f8f9de1804831167b21751a57fef1b
cpapae02 <71434676+cpapae02@users.noreply.github.com>
2021-04-10 02:13:35 +0300

    update api call for events

--- api/models/events.php
+++ api/models/events.php
@@ -12,6 +12,8 @@
 		public $link;
 		public $start_date;
 		public $stop_date;
+		public $start_time;
+		public $stop_time;
 		public $shop_id;
 
 		public function __construct($db)
@@ -36,13 +38,21 @@
 		
 		//CREATE NEW EVENT
 		public function create_event(){
+			date_default_timezone_set('GMT');
+			$start_date = date('N', strtotime(date($this->start_date))); //DAY NUMBER MON=1 ..
+
 			$this->title = htmlspecialchars(strip_tags($this->title));
 			$this->content = htmlspecialchars(strip_tags($this->content));
 			$this->pic = htmlspecialchars(strip_tags($this->pic));
 			$this->link = htmlspecialchars(strip_tags($this->link));
 			$this->start_date = htmlspecialchars(strip_tags($this->start_date));
 			$this->stop_date = htmlspecialchars(strip_tags($this->stop_date));
+			$this->start_time = htmlspecialchars(strip_tags($this->start_time));
+			$this->stop_time = htmlspecialchars(strip_tags($this->stop_time));
 			$this->shop_id = htmlspecialchars(strip_tags($this->shop_id));
+
+			$this->start_date = date('Y-m-d', strtotime($this->start_date)).' '.$this->start_time;
+			$this->stop_date = date('Y-m-d', strtotime($this->stop_date)).' '.$this->stop_time;
 			$sql = 'CALL create_event("'.$this->title.'","'.$this->content.'","'.$this->pic.'","'.$this->link.'","'.$this->start_date.'","'.$this->stop_date.'",'.$this->shop_id.')';
 			
 			$stmt = $this->conn->prepare($sql);
