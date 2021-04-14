3102dd1bd31267273ce8c745084c554829e92c28
MiltiadisTavelis <mtavel01@ucy.ac.cy>
2021-04-14 15:56:13 +0300

    BUG FIX

--- api/models/reservations.php
+++ api/models/reservations.php
@@ -252,7 +252,7 @@
 			if($_SESSION['user_type'] == "m" && ($this->status == "0" || $this->status == "1")){
 				$sql = 'SELECT * FROM RESERVATIONS,SHOPS WHERE RESERVATIONS.shopid = SHOPS.id AND RESERVATIONS.id = '.$this->id.' AND SHOPS.mngid = '.$_SESSION['user_id'];
 			}elseif(($_SESSION['user_type'] == "u" && $this->status == "3") || ($_SESSION['user_type'] == "m" && $this->status == "3")){
-				if(($_SESSION['user_type'] == "m"){
+				if($_SESSION['user_type'] == "m"){
 					$sql = 'SELECT * FROM RESERVATIONS WHERE RESERVATIONS.id = '.$this->id;
 				}else{
 					$sql = 'SELECT * FROM RESERVATIONS WHERE RESERVATIONS.id = '.$this->id.' AND RESERVATIONS.userid = '.$_SESSION['user_id'];
